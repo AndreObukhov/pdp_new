3b6ba6a1faa90e6870f2100e65525dc1378ae197
Andrew Obukhov <andrewrcr@mail.ru>
2018-04-11 17:51:40 +0300

    +RUN

---
+++ cmake-build-debug/CMakeFiles/Emulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Emulator.dir/main.c.obj"
+  "Emulator.pdb"
+  "Emulator.exe"
+  "Emulator.exe.manifest"
+  "libEmulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
